3003ef0d597811cb1436fa64770242a5516e8e6d
ashley7070 <ashley7070@gmail.com>
2014-10-14 20:50:22 +1100

    moving to root package and removing maven

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,14 @@
 package hello;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@RestController
 public class HelloController {
 
     @RequestMapping(value="/", method=RequestMethod.GET)
     public String index() {
-        return "index";
+        return "astalavista";
     }
 }
